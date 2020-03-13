9d334bacf8672c63d1eaa6403920de17fd1a23d8
rekcah-pavi <55050467+rekcah-pavi@users.noreply.github.com>
2020-03-13 16:54:50 +0530

    c

--- userbot/modules/afk.py
+++ userbot/modules/afk.py
@@ -14,70 +14,35 @@ from userbot import (AFKREASON, COUNT_MSG, CMD_HELP, ISAFK, BOTLOG,
                      BOTLOG_CHATID, USERS, PM_AUTO_BAN)
 from userbot.events import register
 
-try:
-    from userbot.modules.sql_helper.globals import gvarstatus, addgvar, delgvar
-    afk_db = True
-except AttributeError:
-    afk_db = False
-
 # ========================= CONSTANTS ============================
 AFKSTR = [
-    "I'm busy right now. Please talk in a bag and when I come back you can just give me the bag!",
-    "I'm away right now. If you need anything, leave a message after the beep:\n`beeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeep`!",
-    "You missed me, next time aim better.",
-    "I'll be back in a few minutes and if I'm not...,\nwait longer.",
-    "I'm not here right now, so I'm probably somewhere else.",
-    "Roses are red,\nViolets are blue,\nLeave me a message,\nAnd I'll get back to you.",
-    "Sometimes the best things in life are worth waiting for…\nI'll be right back.",
-    "I'll be right back,\nbut if I'm not right back,\nI'll be back later.",
-    "If you haven't figured it out already,\nI'm not here.",
-    "Hello, welcome to my away message, how may I ignore you today?",
-    "I'm away over 7 seas and 7 countries,\n7 waters and 7 continents,\n7 mountains and 7 hills,\n7 plains and 7 mounds,\n7 pools and 7 lakes,\n7 springs and 7 meadows,\n7 cities and 7 neighborhoods,\n7 blocks and 7 houses...\n\nWhere not even your messages can reach me!",
-    "I'm away from the keyboard at the moment, but if you'll scream loud enough at your screen, I might just hear you.",
-    "I went that way\n---->",
-    "I went this way\n<----",
-    "Please leave a message and make me feel even more important than I already am.",
-    "I am not here so stop writing to me,\nor else you will find yourself with a screen full of your own messages.",
-    "If I were here,\nI'd tell you where I am.\n\nBut I'm not,\nso ask me when I return...",
-    "I am away!\nI don't know when I'll be back!\nHopefully a few minutes from now!",
-    "I'm not available right now so please leave your name, number, and address and I will stalk you later.",
-    "Sorry, I'm not here right now.\nFeel free to talk to my userbot as long as you like.\nI'll get back to you later.",
-    "I bet you were expecting an away message!",
-    "Life is so short, there are so many things to do...\nI'm away doing one of them..",
-    "I am not here right now...\nbut if I was...\n\nwouldn't that be awesome?",
+    "Hello Sir My owner is offline just leave your message! Thankyou",
+    
 ]
 # =================================================================
 
 
-@register(incoming=True, disable_errors=True)
+@register(incoming=True, disable_edited=True)
 async def mention_afk(mention):
     """ This function takes care of notifying the people who mention you that you are AFK."""
     global COUNT_MSG
     global USERS
     global ISAFK
-    global AFFKREASON
-    ISAFK_SQL = False
-    AFKREASON_SQL = None
-    if afk_db:
-        ISAFK_SQL = gvarstatus("AFK_STATUS")
-        AFKREASON_SQL = gvarstatus("AFK_REASON")
-    EXCUSE = AFKREASON_SQL if afk_db else AFKREASON
     if mention.message.mentioned and not (await mention.get_sender()).bot:
-        if ISAFK or ISAFK_SQL:
+        if ISAFK:
             if mention.sender_id not in USERS:
-                if EXCUSE:
-                    await mention.reply(f"I'm AFK right now.\
-                    \nReason: `{EXCUSE}`")
+                if AFKREASON:
+                    await mention.reply(f"𝓘'𝓶 𝓐𝓕𝓚 𝓻𝓲𝓰𝓱𝓽 𝓷𝓸𝔀.\
+                        \nReason: `{AFKREASON}`")
                 else:
                     await mention.reply(str(choice(AFKSTR)))
                 USERS.update({mention.sender_id: 1})
                 COUNT_MSG = COUNT_MSG + 1
             elif mention.sender_id in USERS:
                 if USERS[mention.sender_id] % randint(2, 4) == 0:
-                    if EXCUSE:
-                        await mention.reply(
-                            f"In case you didn't notice, I'm still AFK.\
-                        \nReason: `{EXCUSE}`")
+                    if AFKREASON:
+                        await mention.reply(f"𝓘'𝓶 𝓢𝓽𝓲𝓵𝓵 𝓐𝓕𝓚.\
+                            \nReason: `{AFKREASON}`")
                     else:
                         await mention.reply(str(choice(AFKSTR)))
                     USERS[mention.sender_id] = USERS[mention.sender_id] + 1
@@ -91,15 +56,8 @@ async def mention_afk(mention):
 async def afk_on_pm(sender):
     """ Function which informs people that you are AFK in PM """
     global ISAFK
-    global AFFKREASON
-    ISAFK_SQL = False
-    AFKREASON_SQL = None
-    if afk_db:
-        ISAFK_SQL = gvarstatus("AFK_STATUS")
-        AFKREASON_SQL = gvarstatus("AFK_REASON")
     global USERS
     global COUNT_MSG
-    EXCUSE = AFKREASON_SQL if afk_db else AFKREASON
     if sender.is_private and sender.sender_id != 777000 and not (
             await sender.get_sender()).bot:
         if PM_AUTO_BAN:
@@ -110,21 +68,20 @@ async def afk_on_pm(sender):
                 apprv = True
         else:
             apprv = True
-        if apprv and (ISAFK or ISAFK_SQL):
+        if apprv and ISAFK:
             if sender.sender_id not in USERS:
-                if EXCUSE:
-                    await sender.reply(f"I'm AFK right now.\
-                    \nReason: `{EXCUSE}`")
+                if AFKREASON:
+                    await sender.reply(f"𝓘'𝓶 𝓐𝓕𝓚 𝓻𝓲𝓰𝓱𝓽 𝓷𝓸𝔀.\
+                    \nReason: `{AFKREASON}`")
                 else:
                     await sender.reply(str(choice(AFKSTR)))
                 USERS.update({sender.sender_id: 1})
                 COUNT_MSG = COUNT_MSG + 1
             elif apprv and sender.sender_id in USERS:
                 if USERS[sender.sender_id] % randint(2, 4) == 0:
-                    if EXCUSE:
-                        await sender.reply(
-                            f"In case you didn't notice, I'm still AFK.\
-                        \nReason: `{EXCUSE}`")
+                    if AFKREASON:
+                        await sender.reply(f"I'm still AFK.\
+                        \nReason: `{AFKREASON}`")
                     else:
                         await sender.reply(str(choice(AFKSTR)))
                     USERS[sender.sender_id] = USERS[sender.sender_id] + 1
@@ -134,30 +91,21 @@ async def afk_on_pm(sender):
                     COUNT_MSG = COUNT_MSG + 1
 
 
-@register(outgoing=True, pattern="^\.afk(?: |$)(.*)", disable_errors=True)
+@register(outgoing=True, pattern="^.afk(?: |$)(.*)", disable_errors=True)
 async def set_afk(afk_e):
     """ For .afk command, allows you to inform people that you are afk when they message you """
     message = afk_e.text
     string = afk_e.pattern_match.group(1)
     global ISAFK
-    global AFFKREASON
-    ISAFK_SQL = False
-    AFKREASON_SQL = None
-    if afk_db:
-        ISAFK_SQL = gvarstatus("AFK_STATUS")
-        AFKREASON_SQL = gvarstatus("AFK_REASON")
+    global AFKREASON
     if string:
-        if afk_db:
-            addgvar("AFK_REASON", string)
         AFKREASON = string
-        await afk_e.edit(f"Going AFK!\
+        await afk_e.edit(f"𝓖𝓸𝓲𝓷𝓰 𝓐𝓯𝓴!\
         \nReason: `{string}`")
     else:
-        await afk_e.edit("Going AFK!")
+        await afk_e.edit("𝓖𝓸𝓲𝓷𝓰 𝓐𝓯𝓴!")
     if BOTLOG:
-        await afk_e.client.send_message(BOTLOG_CHATID, "#AFK\nYou went AFK!")
-    if afk_db:
-        addgvar("AFK_STATUS", True)
+        await afk_e.client.send_message(BOTLOG_CHATID, "#AFK\n𝓨𝓸𝓾 𝔀𝓮𝓷𝓽 𝓐𝓯𝓴!")
     ISAFK = True
     raise StopPropagation
 
@@ -165,21 +113,15 @@ async def set_afk(afk_e):
 @register(outgoing=True)
 async def type_afk_is_not_true(notafk):
     """ This sets your status as not afk automatically when you write something while being afk """
+    global ISAFK
     global COUNT_MSG
     global USERS
-    global ISAFK
-    global AFFKREASON
-    AFKREASON_SQL = None
-    ISAFK_SQL = False
-    if afk_db:
-        ISAFK_SQL = gvarstatus("AFK_STATUS")
-        AFKREASON_SQL = gvarstatus("AFK_REASON")
-    if ISAFK or ISAFK_SQL:
-        if afk_db:
-            delgvar("AFK_STATUS")
-            delgvar("AFK_REASON")
+    global AFKREASON
+    if ISAFK:
         ISAFK = False
-        AFKREASON = None
+        msg = await notafk.respond("𝓘'𝓶 𝓷𝓸 𝓵𝓸𝓷𝓰𝓮𝓻 𝓐𝓯𝓴.")
+        await sleep(1)
+        await msg.delete()
         if BOTLOG:
             await notafk.client.send_message(
                 BOTLOG_CHATID,
@@ -196,6 +138,7 @@ async def type_afk_is_not_true(notafk):
                 )
         COUNT_MSG = 0
         USERS = {}
+        AFKREASON = None
 
 
 CMD_HELP.update({
