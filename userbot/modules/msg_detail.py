30fff9ff8acc5eaffb9c4d9e88ed0e1d9a5e1273
rekcah-pavi <55050467+rekcah-pavi@users.noreply.github.com>
2020-03-14 17:21:54 +0530

    c

--- userbot/modules/msg_detail.py
+++ userbot/modules/msg_detail.py
@@ -63,7 +63,7 @@ async def killdabot(event):
 
 @register(outgoing=True, pattern="^\.restart$")
 async def killdabot(event):
-    await event.edit("`BRB... *PornHub intro*`")
+    await event.edit("`restarting....`")
     if BOTLOG:
         await event.client.send_message(BOTLOG_CHATID, "#RESTART \n"
                                         "Bot Restarted")
