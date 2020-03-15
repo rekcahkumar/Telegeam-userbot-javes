b876de16e2acfcb8eb9db5fc5337a68042fc923d
rekcah-pavi <55050467+rekcah-pavi@users.noreply.github.com>
2020-03-15 12:16:57 +0530

    Update filter.py

--- userbot/modules/filter.py
+++ userbot/modules/filter.py
@@ -31,8 +31,6 @@ async def filter_incoming_handler(handler):
                     msg_o = await handler.client.get_messages(
                         entity=BOTLOG_CHATID, ids=int(trigger.f_mesg_id))
                     await handler.reply(msg_o.message, file=msg_o.media)
-await sleep(60)
-        await msg.delete()
                 elif pro and trigger.reply:
                     await handler.reply(trigger.reply)
     except AttributeError:
