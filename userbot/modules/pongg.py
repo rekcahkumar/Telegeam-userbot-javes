e9b78c0612566d7c6d4d85412d5677af7e972ae1
rekcah-pavi <55050467+rekcah-pavi@users.noreply.github.com>
2020-03-17 08:35:47 +0530

    Update pongg.py

--- userbot/modules/pongg.py
+++ userbot/modules/pongg.py
@@ -1,13 +1,28 @@
-from telethon import events
-from datetime import datetime
-from userbot.events import command
+# Copyright (C) 2019 The Raphielscape Company LLC.
+#
+# Licensed under the Raphielscape Public License, Version 1.c (the "License");
+# you may not use this file except in compliance with the License.
+#
+""" Userbot module containing commands related to the \
+    Information Superhighway (yes, Internet). """
 
-@command(pattern="^.rk")
-async def _(event):
-    if event.fwd_from:
-        return
+from datetime import datetime
+
+from speedtest import Speedtest
+from telethon import functions
+from userbot import CMD_HELP
+from userbot.events import javes
+
+
+
+
+@javes(outgoing=True, pattern="^\.rk$")
+async def pingme(pong):
+    """ For .ping command, ping the userbot from any chat.  """
     start = datetime.now()
-    await event.edit("Pong!")
+    await pong.edit("`Pong!`")
     end = datetime.now()
-    ms = (end - start).microseconds / 1000
-    await event.edit("Pong!\n{}".format(ms))
+    duration = (end - start).microseconds / 1000
+    await pong.edit("`Pong!\n%sms`" % (duration))
+
+
